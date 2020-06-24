13d1f7ed13c9d2e1fdede38a0c4ab83e245cc7d8
Lukas Schmidt <lukasschmidt@fastmail.com>
2020-06-24 12:30:26 +0200

    improves combined trip
    Signed-off-by: Lukas Schmidt <lukasschmidt@fastmail.com>

--- Sources/GTFSPipline/CombinedTrip.swift
+++ Sources/GTFSPipline/CombinedTrip.swift
@@ -13,13 +13,16 @@ public struct CombinedTrip {
     public var stops: [Stop]
     public var stopTimes: [StopTime]
     
-    public init(trip: Trip, stopTimes: [StopTime], stops: [Stop]) {
+    public init(trip: Trip, stopTimes: [StopTime], stops: [Stop]) throws {
         self.trip = trip
         self.stopTimes = stopTimes.filter { $0.tripId == trip.id }
-        self.stops = self.stopTimes.map { time in
-            stops.first(where: { stop in
+        self.stops = try self.stopTimes.map { time in
+            guard let stop = stops.first(where: { stop in
                 stop.id == time.stopId
-            })!
+            }) else {
+                throw NSError(domain: "Could not find stop with id \(time.stopId)", code: 0, userInfo: ["tripshortName": trip.shortName ?? ""])
+            }
+            return stop
         }
     }
 }
