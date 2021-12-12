2e67499f165f608f0c1e455ab9403a1720d08555
Lukas Schmidt <lightsprint09@users.noreply.github.com>
2021-12-12 14:56:49 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/lightsprint09/GTFS", ._branchItem("master")),
+        .package(url: "https://github.com/lightsprint09/GTFS", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
